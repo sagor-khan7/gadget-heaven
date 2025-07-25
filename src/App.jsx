70d560d261809320000640124b4f78b3a4f9938b
sagor-khan7 <sksagor791@gmail.com>
2025-07-25 22:38:33 +0600

    created initial setup with necessary packages

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to Gadget Heaven</h1>
+    </div>
+  );
+};
+
+export default App;
